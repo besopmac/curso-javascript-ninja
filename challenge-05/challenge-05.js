f273381520541e4affa8edeb381906166f56b24c
Bruno Campos <brunosopmac@gmail.com>
2018-12-02 18:05:07 -0200

    Finalizando Challenge #5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myArray = ['Barney', 2, false, null, 'Magali'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function imprimeVetor(vetor) {
+  return vetor;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(imprimeVetor(myArray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -21,17 +23,24 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
+function retornaIndice(vetor, ponteiro) {
+  return vetor[ponteiro];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var novaArray = ['Marina', undefined, [7, 8, 9], { dog: 'Barney' }, true];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(retornaIndice(novaArray, 0));
+console.log(retornaIndice(novaArray, 1));
+console.log(retornaIndice(novaArray, 2));
+console.log(retornaIndice(novaArray, 3));
+console.log(retornaIndice(novaArray, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -48,29 +57,52 @@ propriedades:
 os livros.
 */
 // ?
+function book(nome) {
+  var estante = {
+    '1984': {
+      quantidadePaginas: 356,
+      autor: 'George Orwell',
+      editora: 'LeYa'
+    },
+    'O Imbecil Coletivo': {
+      quantidadePaginas: 459,
+      autor: 'Olavo de Carvalho',
+      editora: 'Saraiva'
+    },
+    'Os Segredos do Lobo': {
+      quantidadePaginas: 340,
+      autor: 'Jordan Belfort',
+      editora: 'Ediouro'
+    }
+  }
+
+  return !nome ? estante : estante[nome];
+}
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var nomeLivro = 'O Imbecil Coletivo';
+console.log(`"O livro ${nomeLivro} tem ${ book(nomeLivro).quantidadePaginas } páginas!"`)
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log(`"O autor do livro ${nomeLivro} é ${ book(nomeLivro).autor }."`)
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log(`"O livro ${nomeLivro} foi publicado pela editora ${ book(nomeLivro).editora }."`)
